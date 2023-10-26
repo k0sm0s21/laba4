016396e94515122e9e0b455ad5181eae61af5223
k0sm0s21 <145329561+k0sm0s21@users.noreply.github.com>
2023-10-26 14:51:41 +0300

    Add files via upload

---
+++ 4.5/4.5/4.5.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    unsigned long long int num1 = 1, num2 = 0, s;
+    int n;
+
+    cout << "Введите количество чисел: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        s = num1 + num2;
+        num1 = num2;
+        num2 = s;
+        cout << s << " \n";
+    }
+
+    return 0;
+}
