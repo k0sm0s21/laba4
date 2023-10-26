ab5dd807cc4a5c40218a27629fa3a7e1dcfb13ac
k0sm0s21 <145329561+k0sm0s21@users.noreply.github.com>
2023-10-26 14:54:26 +0300

    Add files via upload

---
+++ 4.6/4.6/4.6.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    int nizm, ncst;
+    string result = "";
+    int num_at_mom;
+
+    cout << "Введите число: ";
+    cin >> nizm;
+    ncst = nizm;
+    while (nizm != 0) {
+        num_at_mom = nizm % 2;
+        result = to_string(num_at_mom) + result;
+        nizm /= 2; //n=n/2
+    }
+
+    cout << "Число " << ncst << " из системы счисления 10 в системе счисления 2 равно " << result << endl;
+
+    return 0;
+}
